3a7112865204892888200e453e6574d74877caba
MakerZip <makerzip@hanmail.net>
2023-11-20 10:18:03 +0900

    Update gokart.ino

--- gokart/gokart.ino
+++ gokart/gokart.ino
@@ -1,10 +1,13 @@
 const int acceleratorPin = A0;
 const int motorSpeedPin = 9;
 const int motorDirectionPin = 8;
+const int errorPin = A1;
 
 bool toggle = false;
+bool error = false;
 
 void setup() {
+  pinMode(errorPin, INPUT);
   pinMode(acceleratorPin, INPUT);
   pinMode(motorDirectionPin, OUTPUT);
   pinMode(motorSpeedPin, OUTPUT);
@@ -12,18 +15,36 @@ void setup() {
 }
 
 void loop() {
-  int s = map(analogRead(acceleratorPin), 182, 877, 0, 255);
-  Serial.println(s);
+  int s = map(analogRead(acceleratorPin), 180, 874, 0, 255);
+  if (s < 0) {
+    s = 0;
+    Serial.print("ERROR");
+    Serial.print(" ");
+  } else if (s > 256) {
+    s = 0;
+    Serial.print("ERROR");
+    Serial.print(" ");
+  }
 
-  if (s == 0) {
+  // if (s > 22 && s < 35) {
+  //   s = 0;
+  //   Serial.print("ERROR");
+  //   Serial.print(" ");
+  // }
+
+  if (s == 255) {
     toggle = !toggle; // toggle을 반전시킴
   }
+  Serial.print(toggle);
+  Serial.print(" ");
 
   if (toggle == false) {
     digitalWrite(motorDirectionPin, HIGH);
     analogWrite(motorSpeedPin, s);
+    Serial.println(s);
   } else {
     digitalWrite(motorDirectionPin, LOW);
     analogWrite(motorSpeedPin, abs(s));
+    Serial.println(abs(s));
   }
 }
